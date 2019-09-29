e4de19687968ab628ba2bd4b897c9b8385b786d9
yreiss1 <reiss.yuval@gmail.com>
2019-09-28 21:25:48 -0400

    UI for chatscreen and home page

--- lib/views/ChatScreen.dart
+++ lib/views/ChatScreen.dart
@@ -2,13 +2,51 @@ import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   static const routeName = '/chat-screen';
+  final List<Contact> _contacts = [
+    Contact(email: "omeryampel.gmail", fullName: "Omer Yampel"),
+    Contact(email: "omeryampel.gmail", fullName: "Omer Yampel"),
+    Contact(email: "omeryampel.gmail", fullName: "Omer Yampel"),
+    Contact(email: "omeryampel.gmail", fullName: "Omer Yampel"),
+    Contact(email: "omeryampel.gmail", fullName: "Omer Yampel"),
+    Contact(email: "omeryampel.gmail", fullName: "Omer Yampel")
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("The Chat Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat"),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 4.0),
+        itemBuilder: (context, index) {
+          return Column(
+            children: <Widget>[
+              _ContactListItem(_contacts[index]),
+              Divider(),
+            ],
+          );
+        },
+        itemCount: _contacts.length,
       ),
     );
   }
+}
+
+class _ContactListItem extends ListTile {
+  _ContactListItem(Contact contact)
+      : super(
+            title: new Text(contact.fullName),
+            subtitle: new Text(contact.email),
+            leading: new CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(
+                    "https://thumbnailer.mixcloud.com/unsafe/1200x628/tmp/7/4/2/8/fac1-7b75-4a97-a54c-02d8d853fa48")));
+}
+
+class Contact {
+  final String fullName;
+  final String email;
+
+  const Contact({this.fullName, this.email});
 }
--- lib/views/HomeScreen.dart
+++ lib/views/HomeScreen.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:jammerz/views/LoginScreen.dart';
+import './ChatScreen.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home-screen';
+
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.music_note),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 4.0,
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.chat_bubble),
+              onPressed: () =>
+                  Navigator.pushNamed(context, ChatScreen.routeName),
+            ),
+            IconButton(
+              icon: Icon(Icons.group),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text("Jammerz"),
         actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {},
+          ),
           IconButton(
             icon: Icon(
               Icons.exit_to_app,
@@ -20,9 +51,47 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: Container(
-        child: Center(
-          child: Text("This is the home screen"),
-        ),
+        height: screenSize.height,
+        width: screenSize.width,
+        child: SingleChildScrollView(
+            child: Column(
+          children: <Widget>[
+            Card(
+              elevation: 5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(
+                          "https://thumbnailer.mixcloud.com/unsafe/1200x628/tmp/7/4/2/8/fac1-7b75-4a97-a54c-02d8d853fa48"),
+                    ),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        child: Text(
+                          "Omer Yampel",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Text(
+                          "Guitarist",
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
